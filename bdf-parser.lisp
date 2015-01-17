5be8ae742000d8b8b630184b927100dcde536337
Stuart Olsen <so1132@txstate.edu>
2015-01-16 21:28:16 -0600

    Rearrange rules

--- bdf-parser.lisp
+++ bdf-parser.lisp
@@ -71,13 +71,37 @@
   (:start-symbol bdf)
   (:terminals #.(list* :newline :string +keywords+))
 
-  (bdf  (startfont-line global-lines endfont-line  #'*list-drop))
+  (bdf               (startfont-line       global-lines     endfont-line        #'*list-drop))
+  (properties-block  (startproperties-line property-lines   endproperties-line  #'*list-drop))
+  (chars-block       (chars-line           startchar-blocks                     #'*list))
+  (startchar-block   (startchar-line       charprop-lines   endchar-line        #'*list-drop))
+  (bitmap-block      (bitmap-line          hex-lines                            #'*list))
 
-  (startfont-line  (STARTFONT :string :newline  #'droplast))
-  (endfont-line    (ENDFONT           :newline  #'droplast))
+  (startfont-line        (STARTFONT       :string                         :newline  #'droplast))
+  (endfont-line          (ENDFONT                                         :newline  #'droplast))
+  (comment-line          (COMMENT         :string                         :newline  #'droplast))
+  (contentversion-line   (CONTENTVERSION  :string                         :newline  #'droplast))
+  (font-line             (FONT            :string                         :newline  #'droplast))
+  (size-line             (SIZE            :string :string :string         :newline  #'droplast))
+  (fontboundingbox-line  (FONTBOUNDINGBOX :string :string :string :string :newline  #'droplast))
+  (metricsset-line       (METRICSSET      :string                         :newline  #'droplast))
+  (swidth-line           (SWIDTH          :string :string                 :newline  #'droplast))
+  (dwidth-line           (DWIDTH          :string :string                 :newline  #'droplast))
+  (swidth1-line          (SWIDTH1         :string :string                 :newline  #'droplast))
+  (dwidth1-line          (DWIDTH1         :string :string                 :newline  #'droplast))
+  (vvector-line          (VVECTOR         :string :string                 :newline  #'droplast))
+  (startproperties-line  (STARTPROPERTIES :string                         :newline  #'droplast))
+  (property-line         (:string         :string                         :newline  #'droplast))
+  (endproperties-line    (ENDPROPERTIES                                   :newline  #'droplast))
+  (chars-line            (CHARS           :string                         :newline  #'droplast))
+  (startchar-line        (STARTCHAR       :string                         :newline  #'droplast))
+  (endchar-line          (ENDCHAR                                         :newline  #'droplast))
+  (encoding-line         (ENCODING        :string                         :newline  #'droplast)
+                         (ENCODING        :string :string                 :newline  #'droplast))
+  (bbx-line              (BBX             :string :string :string :string :newline  #'droplast))
+  (bitmap-line           (BITMAP                                          :newline  #'droplast))
+  (hex-line              (:string                                         :newline  #'first*))
 
-  (global-lines  (global-line global-lines  #'list*)
-                 ())
   (global-line   comment-line
                  contentversion-line
                  font-line
@@ -92,35 +116,6 @@
                  properties-block
                  chars-block)
 
-  (comment-line          (COMMENT         :string                         :newline  #'droplast))
-  (contentversion-line   (CONTENTVERSION  :string                         :newline  #'droplast))
-  (font-line             (FONT            :string                         :newline  #'droplast))
-  (size-line             (SIZE            :string :string :string         :newline  #'droplast))
-  (fontboundingbox-line  (FONTBOUNDINGBOX :string :string :string :string :newline  #'droplast))
-  (metricsset-line       (METRICSSET      :string                         :newline  #'droplast))
-  (swidth-line           (SWIDTH          :string :string                 :newline  #'droplast))
-  (dwidth-line           (DWIDTH          :string :string                 :newline  #'droplast))
-  (swidth1-line          (SWIDTH1         :string :string                 :newline  #'droplast))
-  (dwidth1-line          (DWIDTH1         :string :string                 :newline  #'droplast))
-  (vvector-line          (VVECTOR         :string :string                 :newline  #'droplast))
-
-  (properties-block      (startproperties-line property-lines endproperties-line  #'*list-drop))
-  (startproperties-line  (STARTPROPERTIES :string :newline                        #'droplast))
-  (property-line         (:string         :string :newline                        #'droplast))
-  (endproperties-line    (ENDPROPERTIES           :newline                        #'droplast))
-  (property-lines        (property-line property-lines                            #'list*)
-                         ())
-
-  (chars-block       (chars-line startchar-blocks       #'*list))
-  (chars-line        (CHARS :string :newline            #'droplast))
-  (startchar-blocks  (startchar-block startchar-blocks  #'list*)
-                     ())
-
-  (startchar-block  (startchar-line charprop-lines endchar-line  #'*list-drop))
-  (startchar-line   (STARTCHAR :string :newline                  #'droplast))
-  (endchar-line     (ENDCHAR           :newline                  #'droplast))
-  (charprop-lines   (charprop-line charprop-lines                #'list*)
-                    ())
   (charprop-line    encoding-line
                     swidth-line
                     dwidth-line
@@ -130,15 +125,11 @@
                     bbx-line
                     bitmap-block)
 
-  (encoding-line  (ENCODING :string                         :newline  #'droplast)
-                  (ENCODING :string :string                 :newline  #'droplast))
-  (bbx-line       (BBX      :string :string :string :string :newline  #'droplast))
-
-  (bitmap-block  (bitmap-line hex-lines  #'*list))
-  (bitmap-line   (BITMAP  :newline       #'droplast))
-  (hex-line      (:string :newline       #'first*))
-  (hex-lines     (hex-line hex-lines     #'list*)
-                 ()))
+  (global-lines      (global-line     global-lines      #'list*)  ())
+  (property-lines    (property-line   property-lines    #'list*)  ())
+  (startchar-blocks  (startchar-block startchar-blocks  #'list*)  ())
+  (charprop-lines    (charprop-line   charprop-lines    #'list*)  ())
+  (hex-lines         (hex-line        hex-lines         #'list*)  ()))
 
 (defun bdf-parse (stream)
   (parse-with-lexer (bdf-lexer stream) *bdf-parser*))
